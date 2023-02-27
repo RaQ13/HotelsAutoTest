7f2ce5d1232a70420292c9bed9e48badf3b19328
RaQ13 <lukasz46@op.pl>
2023-02-27 10:49:39 +0100

    firs commit

---
+++ src/test/java/HotelSearch.java
@@ -0,0 +1,22 @@
+import org.checkerframework.checker.units.qual.C;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class HotelSearch {
+
+    @Test
+    public void searchHotel() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); //półśrodek dla czekania na element
+//        driver.manage().window().maximize();
+        driver.get("http://www.kurs-selenium.pl/demo/");
+        driver.findElement(By.xpath("//span[text()='Search by Hotel or City Name']")).click();
+        driver.findElement(By.xpath("//div[@id='select2-drop']//input")).sendKeys("Dubai");
+//        Thread.sleep(3000); //krok 1, okazuje sie że wystarczyło poczekać na element
+        driver.findElement(By.xpath("//span[@class='select2-match' and text()='Dubai']")).click();
+    }
+}
