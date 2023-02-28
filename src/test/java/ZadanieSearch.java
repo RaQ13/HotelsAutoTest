d9d963dcbfcd300942a3b187bd3b7040e3365e8d
RaQ13 <lukasz46@op.pl>
2023-02-28 11:46:18 +0100

    zadanie szukanie hotelu, edycja daty, asercja

---
+++ src/test/java/ZadanieSearch.java
@@ -0,0 +1,48 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class ZadanieSearch {
+
+    @Test
+    public void searchHotels() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("http://www.kurs-selenium.pl/demo/");
+
+        //pobiera aktualną date w formacie dd/MM/yyyy
+        String pattern = "dd/MM/yyyy";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+        String dateFlightOut = simpleDateFormat.format(new Date());
+
+        //zwiększa aktualną datę o 4 dni
+        Date dateBack = new Date();
+        Calendar c = Calendar.getInstance();
+        c.setTime(dateBack);
+        c.add(Calendar.DATE, 4);
+        dateBack = c.getTime();
+        String dateFlightBack = simpleDateFormat.format(dateBack);
+
+//        System.out.println(dateFlightOut);
+//        System.out.println(dateFlightBack);
+
+        driver.findElement(By.name("checkin")).sendKeys(dateFlightOut);
+        driver.findElement(By.name("checkout")).sendKeys(dateFlightBack);
+        driver.findElement(By.name("travellers")).click();
+        driver.findElement(By.id("adultPlusBtn")).click();
+        driver.findElement(By.id("childPlusBtn")).click();
+        driver.findElement(By.xpath("//button[@type='submit' and text()=' Search']")).click();
+        WebElement heading = driver.findElement(By.xpath("//h2[@class='text-center' and text()='No Results Found']"));
+        Assert.assertEquals(heading.getText(), "No Results Found");
+    }
+}
