30bb733be9c13b71728dd888d911bb0a21d04ad4
RaQ13 <lukasz46@op.pl>
2023-03-23 11:20:40 +0100

    generator raportów html reporter

--- src/test/java/patterns/pl/travelers/tests/BaseTest.java
+++ src/test/java/patterns/pl/travelers/tests/BaseTest.java
@@ -1,11 +1,15 @@
 package patterns.pl.travelers.tests;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import org.checkerframework.checker.units.qual.C;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 import patterns.pl.travelers.utils.DriverFactory;
 
 import java.io.IOException;
@@ -14,6 +18,23 @@ import java.util.concurrent.TimeUnit;
 public class BaseTest {
 
     protected WebDriver driver;
+
+    //deklaracja pól dla możliwości skorzytstania z metody flush
+    //statyczne bo nei są zależne od obiektu klasy tylko powiązane z daną klasą
+    public static ExtentHtmlReporter htmlReporter;
+    public static ExtentReports extentReports;
+
+    @BeforeSuite
+    public void beforeSuite() {
+        htmlReporter = new ExtentHtmlReporter("index.html");
+        extentReports = new ExtentReports();
+        extentReports.attachReporter(htmlReporter);
+    }
+    @AfterSuite
+    public void afterSuite() {
+        htmlReporter.flush();
+        extentReports.flush();
+    }
     @BeforeMethod
     public void setup() throws IOException {
         driver = DriverFactory.getDriver();
--- src/test/java/patterns/pl/travelers/tests/HotelSearchTest.java
+++ src/test/java/patterns/pl/travelers/tests/HotelSearchTest.java
@@ -1,5 +1,8 @@
 package patterns.pl.travelers.tests;
 
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.MediaEntityBuilder;
+import com.aventstack.extentreports.Status;
 import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
@@ -60,24 +63,31 @@ public class HotelSearchTest extends BaseTest {
 
     /** Domyślny bez Fluent */
     @Test
-    public void searchHotelTest() {
-
+    public void searchHotelTest() throws IOException {
+        //html reporter
+        ExtentTest test = extentReports.createTest("Search Hotel Test");
         HotelSearchPage hotelSearchPage = new HotelSearchPage(driver); //przekazany driver do PageFactory
         hotelSearchPage.setCityName("Dubai");
+        test.log(Status.PASS, "Setting city done");
         hotelSearchPage.setDates("20/10/2023", "26,10,2023");
+        test.log(Status.PASS, "Setting dates done");
         hotelSearchPage.setTravellers(1, 1);
+        test.log(Status.PASS, "Setting travellers done");
         hotelSearchPage.performSearch();
+        test.log(Status.PASS, "Performing search done");
 
         driver.findElement(By.xpath("//button[text()=' Search']")).click();
 
         ResultsPage resultsPage = new ResultsPage(driver);
-
         List<String> hotelNames = resultsPage.getHotelNames();
 
         Assert.assertEquals(hotelNames.get(0), "Jumeirah Beach Hotel");
         Assert.assertEquals(hotelNames.get(1),"Oasis Beach Tower");
         Assert.assertEquals(hotelNames.get(2), "Rose Rayhaan Rotana");
         Assert.assertEquals(hotelNames.get(3), "Hyatt Regency Perth");
+        test.log(Status.PASS, "Assertions passed");
+        //dodanie screena do raportu
+        test.log(Status.PASS, "Screenshot", MediaEntityBuilder.createScreenCaptureFromPath("src/test/resources/screenshots/screen.png").build());
     }
 
     @Test
@@ -108,9 +118,7 @@ public class HotelSearchTest extends BaseTest {
 
     @Test(dataProvider = "excelData")
     public void searchHotelTest2WithDataProvider(String city, String hotel) {
-
         HotelSearchPage hotelSearchPage = new HotelSearchPage(driver); //przekazany driver do PageFactory
-
         List<String> hotelNames = hotelSearchPage.setCityName(city)
                 .setDates("20/10/2023", "26,10,2023")
                 .setTravellers(1,1)
